0dc540950a9fee012d3a6719b5117918b52062ee
Bobeer95 <artur.buja2@gmail.com>
2022-01-03 22:00:38 +0100

    Adding carousel section with adding style in scss compiling to css and adding js.

--- development/js/index.js
+++ development/js/index.js
@@ -11,3 +11,35 @@ navBtn.forEach(button => {
         button.classList.add('active');
     })
 })
+
+// Carousel
+
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0;
+    }
+    setActiveSlide();
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+    if (activeSlide < 0 ) {
+        activeSlide = slides.length - 1;
+    }
+    setActiveSlide();
+})
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+
+    })
+    slides[activeSlide].classList.add('active')
+}
