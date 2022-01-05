71c03cd42a0e195e23f9360dd36b91bbb93043e8
andro2ss <a.brzeski@hotmail.com>
2022-01-05 03:31:15 -0800

    Jumping between menu windows added

--- development/js/app.js
+++ development/js/app.js
@@ -3,8 +3,51 @@ const appMenu = document.getElementById("sectionMenu");
 const menuItems = appMenu.getElementsByClassName("menu__item");
 for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener("click", function() {
-        let current = document.getElementsByClassName(" menu__item--active");
-        current[0].className = current[0].className.replace(" menu__item--active", "");
+        let currentMenu = document.getElementsByClassName(" menu__item--active");
+        currentMenu[0].className = currentMenu[0].className.replace(" menu__item--active", "");
         this.className += " menu__item--active";
+
+        let currentSection = document.getElementsByClassName(" active");
+        currentSection[0].className = currentSection[0].className.replace(" active", "")
+        if (i===0){
+            document.getElementById("containerPulpit").className += " active";
+        } else if (i===1){
+            document.getElementById("containerRecipes").className += " active";
+        } else {
+            document.getElementById("containerSchedules").className += " active";
+        }
     });
 }
+//******************************************
+
+
+//Funkcja wciagajaca inny HTML do DIVA
+function includeHTML() {
+    let z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+};
+includeHTML();
+//*****************************************
