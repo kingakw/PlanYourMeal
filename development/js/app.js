28aaac43c3b29c7126784e97049e9b246822919b
andro2ss <a.brzeski@hotmail.com>
2022-01-05 07:18:58 -0800

    task done - layout ok, we can add elements by function addRecipe() btn + adding test recipe

--- development/js/app.js
+++ development/js/app.js
@@ -2,19 +2,32 @@
 const appMenu = document.getElementById("sectionMenu");
 const menuItems = appMenu.getElementsByClassName("menu__item");
 for (let i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener("click", function() {
+    menuItems[i].addEventListener("click", function () {
         let currentMenu = document.getElementsByClassName(" menu__item--active");
         currentMenu[0].className = currentMenu[0].className.replace(" menu__item--active", "");
         this.className += " menu__item--active";
 
         let currentSection = document.getElementsByClassName(" active");
         currentSection[0].className = currentSection[0].className.replace(" active", "")
-        if (i===0){
+        if (i === 0) {
             document.getElementById("containerPulpit").className += " active";
-        } else if (i===1){
+        } else if (i === 1) {
             document.getElementById("containerRecipes").className += " active";
+
+            //Skrypty dla strony recipes html ponizej
+
+            const addRecip = document.getElementById("btnPlus");
+            addRecip.addEventListener("click", function () {
+                addRecipe()
+            })
+            // KONIEC skryptow *******************************************************
         } else {
             document.getElementById("containerSchedules").className += " active";
+            //Skrypty dla strony schedules html ponizej
+
+
+            // KONIEC skryptow *******************************************************
+
         }
     });
 }
@@ -33,10 +46,14 @@ function includeHTML() {
         if (file) {
             /*make an HTTP request using the attribute value as the file name:*/
             xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+            xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
                     /*remove the attribute, and call this function once more:*/
                     elmnt.removeAttribute("w3-include-html");
                     includeHTML();
@@ -50,4 +67,30 @@ function includeHTML() {
     }
 };
 includeHTML();
+
 //*****************************************
+
+
+// Dodawanie pozycji w tabeli z przepisami. Podajemy Nazwe przepisu i opis jak wykonac. ID wyciaga samo
+function addRecipe(recipeName = "test name", recipeDesc = "test description") {
+    let recipeID = document.getElementsByClassName("recipe__id");
+    let newID = recipeID.length + 1;
+    let myHtmlContent = `<td class="recipe__id">${newID}</td>
+                        <td class="recipe__name">${recipeName}</td>
+                        <td class="recipe__description">${recipeDesc}</td>
+                        <td class="recipe__action">
+                            <button class="btn btn__edit">
+                                <i class="far fa-edit fa-1x"></i>
+                            </button>
+                            <button class="btn btn__trash">
+                                <i class="far fa-trash-alt fa-1x"></i>
+                            </button>
+                        </td>`
+
+    let tableRef = document.getElementById("recipe__list");
+    let newRow = tableRef.insertRow(tableRef.rows.length);
+    newRow.innerHTML = myHtmlContent;
+}
+
+//************************************************************
+
